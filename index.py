eecb8aeb6c1056c15ca5de2718fb45d1d80ad296
Dimitar Georgiev <d.georgiev0348@gmail.com>
2024-06-04 13:15:45 +0200

    db connection

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 db = SQLAlchemy()
 
-DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@unmute-mysql-1:3306/{os.getenv('DB')}"
+DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@mysql-1:3306/{os.getenv('DB')}"
 
 app = Flask(__name__)
 
