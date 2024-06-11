4818c8d7b8540d8f45b2106fa3a1e3e561e8eddf
Dimitar Georgiev <d.georgiev0348@gmail.com>
2024-06-12 01:06:15 +0200

    custom gesture recognition

--- static/recognition.js
+++ static/recognition.js
@@ -8,14 +8,11 @@ let webcamRunning = false;
 const videoHeight = "360px";
 const videoWidth = "480px";
 
-// Before we can use HandLandmarker class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
 const createGestureRecognizer = async () => {
     const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm");
     gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
         baseOptions: {
-            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+            modelAssetPath: "https://res.cloudinary.com/dx4dnz8n2/raw/upload/v1718146948/mgdurbdgeml2rdiewfbj.task",
             delegate: "GPU"
         },
         runningMode: runningMode
